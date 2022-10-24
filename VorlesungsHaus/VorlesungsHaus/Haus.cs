652fdf52829617ac3f2d5ae5770d9622b4afb86f
CheaterJoe <josef.griesbauer@hs-kempten.de>
2022-10-24 09:30:46 +0200

    Vorlesungshaus Stand 24.10.2022

---
+++ VorlesungsHaus/VorlesungsHaus/Haus.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VorlesungsHaus
+{
+    public class Haus
+    {
+        float _StromV;
+        float _WasserV;
+        float _HeizungsV;
+
+        bool _HeizungAn;
+        bool _StromAn;
+
+        public Haus (float strom)
+        {
+            _StromV = strom;
+        }
+
+        public float StromV
+        {
+            get { return _StromV; }
+            /*set 
+            {
+                if (value < 2000.0)
+                {
+                    _StromV = value;
+                }
+            }*/
+        }
+
+        public float WasserV
+        { get { return _WasserV; } }
+
+        public float HeizungsV
+        { get { return _HeizungsV; } }
+
+        public bool StromAn
+        {
+            get { return _StromAn; }
+            set
+            {
+                _StromAn = value;
+                if (!value)
+                {
+                    _HeizungAn = false;
+                }
+            }
+        }
+
+        public bool HeizungAn
+        { get { return _HeizungAn; }
+            set
+            {
+                if (_StromAn)
+                {
+                    _HeizungAn=true;
+                }
+            }
+        }
+
+        public float WasserverbrauchÜberZeit (float Stunden)
+        {
+            return Stunden * _WasserV;
+        }
+
+        /*Das gleiche wie oben nur anders
+        public float getStromV ()
+        {
+            return _StromV;
+        }
+        
+        public void setStromV(float stromV)
+        {
+            _StromV = stromV;
+        }*/
+    }
+}
---
+++ VorlesungsHaus/VorlesungsHaus/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+VorlesungsHaus.Haus myHouse1 = new VorlesungsHaus.Haus(45);
+VorlesungsHaus.Haus myHouse2 = new VorlesungsHaus.Haus(66);
+
+Console.WriteLine("Hello, World! {0:F}", myHouse1.StromV);
+myHouse1.StromAn = false;
+Console.WriteLine(myHouse1.HeizungAn);
+myHouse1.HeizungAn = true;
+Console.WriteLine(myHouse1.HeizungAn);
+myHouse1.StromAn = true;
+myHouse1.HeizungAn = true;
+Console.WriteLine(myHouse1.HeizungAn);

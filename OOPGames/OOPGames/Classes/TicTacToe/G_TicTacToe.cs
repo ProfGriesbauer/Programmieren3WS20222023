1bf214922ffc32d96299377df67283b7e51b94ed
Kkneiphs <kilian.kneip@stud.hs-kempten.de>
2022-11-09 10:46:59 +0100

    Start dynamisches Spielfeld, Verstehung Linien-Painter :)

--- OOPGames/OOPGames/Classes/TicTacToe/G_TicTacToe.cs
+++ OOPGames/OOPGames/Classes/TicTacToe/G_TicTacToe.cs
@@ -3,10 +3,90 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows;
 
-namespace OOPGames.Classes.TicTacToe
+namespace OOPGames
 {
-    internal class G_TicTacToe
+    public class G_TicTacToePaint : BaseTicTacToePaint
     {
+        public override string Name { get { return "G_TicTacToePaint"; } }
+
+        public override void PaintTicTacToeField(Canvas canvas, ITicTacToeField currentField)
+        {
+            canvas.Children.Clear();
+            Color bgColor = Color.FromRgb(0, 0, 0);
+            canvas.Background = new SolidColorBrush(bgColor);
+            Color lineColor = Color.FromRgb(255, 0, 0);
+            Brush lineStroke = new SolidColorBrush(lineColor);
+            Color XColor = Color.FromRgb(0, 255, 0);
+            Brush XStroke = new SolidColorBrush(XColor);
+            Color OColor = Color.FromRgb(0, 0, 255);
+            Brush OStroke = new SolidColorBrush(OColor);
+
+            Line l1 = new Line() { X1 = 120, Y1 = 0, X2 = 120, Y2 = 428, Stroke = lineStroke, StrokeThickness = 3.0 };
+            canvas.Children.Add(l1);
+            //Line l2 = new Line() { X1 = 220, Y1 = 20, X2 = 220, Y2 = 320, Stroke = lineStroke, StrokeThickness = 3.0 };
+           // canvas.Children.Add(l2);
+            //Line l3 = new Line() { X1 = 20, Y1 = 120, X2 = 320, Y2 = 120, Stroke = lineStroke, StrokeThickness = 3.0 };
+            //canvas.Children.Add(l3);
+            //Line l4 = new Line() { X1 = 20, Y1 = 220, X2 = 320, Y2 = 220, Stroke = lineStroke, StrokeThickness = 3.0 };
+            //canvas.Children.Add(l4);
+            //Line l5 = new Line() { X1 = 20, Y1 = 220, X2 = 320, Y2 = 220, Stroke = lineStroke, StrokeThickness = 3.0 };
+            //canvas.Children.Add(l5);
+            //Line l6 = new Line() { X1 = 20, Y1 = 220, X2 = 320, Y2 = 220, Stroke = lineStroke, StrokeThickness = 3.0 };
+           // canvas.Children.Add(l6);
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (currentField[i, j] == 1)
+                    {
+                        Line X1 = new Line() { X1 = 20 + (j * 100), Y1 = 20 + (i * 100), X2 = 120 + (j * 100), Y2 = 120 + (i * 100), Stroke = XStroke, StrokeThickness = 3.0 };
+                        canvas.Children.Add(X1);
+                        Line X2 = new Line() { X1 = 20 + (j * 100), Y1 = 120 + (i * 100), X2 = 120 + (j * 100), Y2 = 20 + (i * 100), Stroke = XStroke, StrokeThickness = 3.0 };
+                        canvas.Children.Add(X2);
+                    }
+                    else if (currentField[i, j] == 2)
+                    {
+                        Ellipse OE = new Ellipse() { Margin = new Thickness(20 + (j * 100), 20 + (i * 100), 0, 0), Width = 100, Height = 100, Stroke = OStroke, StrokeThickness = 3.0 };
+                        canvas.Children.Add(OE);
+                    }
+                }
+            }
+        }
+    }
+    public class G_TicTacToeField : BaseTicTacToeField
+    {
+        static int _size = 3;
+
+
+        int[,] _Field = new int[_size, _size];
+
+        public override int this[int r, int c]
+        {
+            get
+            {
+                if (r >= 0 && r < 3 && c >= 0 && c < 3)
+                {
+                    return _Field[r, c];
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+
+            set
+            {
+                if (r >= 0 && r < 3 && c >= 0 && c < 3)
+                {
+                    _Field[r, c] = value;
+                }
+            }
+        }
     }
 }

df41b6d4c8541b933c57ec4b15d17a6504e6ce44
BuildTools <unconfigured@null.spigotmc.org>
2022-11-13 17:25:17 +0100

    Renamed

---
+++ OOPGames/OOPGames/Classes/Gruppe_D/I_PainterD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace OOPGames
+{
+    public class IPaintD : B
+}

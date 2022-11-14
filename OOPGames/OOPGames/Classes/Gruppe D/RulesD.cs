e312bb404f3b9bc97eacd40d21c5419df7d55587
Felix Geiger <felixgeiger18@gmail.com>
2022-11-15 00:14:45 +0100

    Anfang Rules

--- OOPGames/OOPGames/Classes/Gruppe D/RulesD.cs
+++ OOPGames/OOPGames/Classes/Gruppe D/RulesD.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace OOPGames
 {
-    internal class RulesD
+    public class RulesD : ITicTacToeRules
+
     {
+        TicTacToeField _KrassesFeld = new TicTacToeField();
+        public string Name { get { return "DieKrassenRegeln"; } }
+
+        public IGameField CurrentField { get { return _KrassesFeld; } }
+
+        public bool MovesPossible => throw new NotImplementedException();
+
+        public ITicTacToeField TicTacToeField => throw new NotImplementedException();
+
+        public int CheckIfPLayerWon()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearField()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DoMove(IPlayMove move)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DoTicTacToeMove(ITicTacToeMove move)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

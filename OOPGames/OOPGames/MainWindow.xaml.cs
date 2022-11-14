50513f0f5cf77e5f648dcb7e78fbb2ed7faf0a99
Johannes Geiger <johannesgeiger27@gmail.com>
2022-11-15 00:02:33 +0100

    Neuer Painter

--- OOPGames/OOPGames/MainWindow.xaml.cs
+++ OOPGames/OOPGames/MainWindow.xaml.cs
@@ -43,7 +43,8 @@ namespace OOPGames
             OOPGamesManager.Singleton.RegisterPainter(new H_TicTacToePaint());
             OOPGamesManager.Singleton.RegisterPainter(new TTTPaint());
             OOPGamesManager.Singleton.RegisterPainter(new PainterI());
-            OOPGamesManager.Singleton.RegisterPainter(new K_Painter_LB());
+            OOPGamesManager.Singleton.RegisterPainter(new K_Painter_JG());
+            //OOPGamesManager.Singleton.RegisterPainter(new K_Painter_LB());
             //OOPGamesManager.Singleton.RegisterPainter(new C_Painter());
             //Rules
             OOPGamesManager.Singleton.RegisterRules(new TicTacToeRules());

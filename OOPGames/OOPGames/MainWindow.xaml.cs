7bedd5900738da7d6c985204212ecf0372ea363e
JNKEI <eisenberger.jannik@gmail.com>
2022-11-14 12:07:35 +0100

    Regeln geaddet Regeln in MainWindow eingefügt

--- OOPGames/OOPGames/MainWindow.xaml.cs
+++ OOPGames/OOPGames/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using OOPGames.Classes.Gruppe_B;
+using OOPGames.Classes.Gruppe_D;
+using OOPGames.Classes.Gruppe_K;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -34,7 +37,14 @@ namespace OOPGames
             //Painters
             OOPGamesManager.Singleton.RegisterPainter(new TicTacToePaint()); 
             OOPGamesManager.Singleton.RegisterPainter(new TicTacToePaint());
+<<<<<<< HEAD
             OOPGamesManager.Singleton.RegisterPainter(new GJ_TicTacToePaint());
+=======
+            OOPGamesManager.Singleton.RegisterPainter(new K_Painter_MS());
+            OOPGamesManager.Singleton.RegisterPainter(new B_Painter());
+            OOPGamesManager.Singleton.RegisterPainter(new H_TicTacToePaint());
+            OOPGamesManager.Singleton.RegisterPainter(new PainterD());
+>>>>>>> 9b08bf975b0f5c874bfc05ca042c7785c91bc0bc
             //Rules
             OOPGamesManager.Singleton.RegisterRules(new TicTacToeRules());
             OOPGamesManager.Singleton.RegisterRules(new GJ_TicTacToeRules());

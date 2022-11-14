a8dabe694b10daed07eb04b08bd7d3e2d21f5a36
Felix2299 <fel.burger@web.de>
2022-11-14 15:04:09 +0100

    Erste Änderungen für Painter

---
+++ OOPGames/OOPGames/Classes/Gruppe E/E-BaseTicTacToe.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace OOPGames.Classes.Gruppe_E
+{
+    public abstract class BaseTicTacToePaint : IPaintTicTacToe
+    {
+        public abstract string Name { get; }
+
+        public abstract void PaintTicTacToeField(Canvas canvas, ITicTacToeField currentField);
+
+        public void PaintGameField(Canvas canvas, IGameField currentField)
+        {
+            if (currentField is ITicTacToeField)
+            {
+                PaintTicTacToeField(canvas, (ITicTacToeField)currentField);
+            }
+        }
+    }
+
+    public abstract class BaseTicTacToeField : ITicTacToeField
+    {
+        public abstract int this[int r, int c] { get; set; }
+
+        public bool CanBePaintedBy(IPaintGame painter)
+        {
+            return painter is IPaintTicTacToe;
+        }
+    }
+}

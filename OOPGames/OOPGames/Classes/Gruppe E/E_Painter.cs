d5308a8cc6f0ebb1b28c86948950da274c10d9e3
test <test@test.com>
2022-11-14 15:51:12 +0100

    änderung

--- OOPGames/OOPGames/Classes/Gruppe E/E_Painter.cs
+++ OOPGames/OOPGames/Classes/Gruppe E/E_Painter.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace OOPGames.Classes.Gruppe_E
 {
-    internal class Painter
-    {
+    public class Painter: IPaintTicTacToe    {
+        public string Name { get; set; }
+
+        public void PaintTicTacToeField(Canvas canvas, ITicTacToeField currentField)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PaintGameField(Canvas canvas, IGameField currentField)
+        {PaintTicTacToeField(canvas, ())
+            throw new NotImplementedException();
+        }
+
+        public 
     }
 }

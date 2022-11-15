ba01e1618bf7b846f3cbf2d4ab2ff6d256444401
Luca Besemer <luca.besemer@stud.hs-kempten.de>
2022-11-15 08:30:37 +0100

    Painter wieder hinzugefügt, da dieser nach dem Push-Fehler in der Übungsstunde nicht gepusht wurde

--- OOPGames/OOPGames/MainWindow.xaml.cs
+++ OOPGames/OOPGames/MainWindow.xaml.cs
@@ -44,13 +44,14 @@ namespace OOPGames
             OOPGamesManager.Singleton.RegisterPainter(new TTTPaint());
             OOPGamesManager.Singleton.RegisterPainter(new PainterI());
             OOPGamesManager.Singleton.RegisterPainter(new K_Painter_JG());
+            OOPGamesManager.Singleton.RegisterPainter(new C_Painter());
             //OOPGamesManager.Singleton.RegisterPainter(new K_Painter_LB());
-            //OOPGamesManager.Singleton.RegisterPainter(new C_Painter());
             //Rules
             OOPGamesManager.Singleton.RegisterRules(new TicTacToeRules());
             OOPGamesManager.Singleton.RegisterRules(new GC_TicTacToeRules());
             //Players
             OOPGamesManager.Singleton.RegisterPlayer(new TicTacToeHumanPlayer());
+            OOPGamesManager.Singleton.RegisterPlayer(new C_TicTacToeHumanPlayer());
             OOPGamesManager.Singleton.RegisterPlayer(new TicTacToeComputerPlayer());
 
             InitializeComponent();

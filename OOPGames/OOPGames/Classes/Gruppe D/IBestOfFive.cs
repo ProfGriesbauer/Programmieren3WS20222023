b0e6b4aa566251b4d7e7fb85e6640cb48d74a850
Lukas <Hallo>
2022-11-21 20:48:26 +0100

    verschönert D

--- OOPGames/OOPGames/Classes/Gruppe D/IBestOfFive.cs
+++ OOPGames/OOPGames/Classes/Gruppe D/IBestOfFive.cs
@@ -9,8 +9,9 @@ namespace OOPGames.Classes.Gruppe_D
     public interface ITicTacToeBOFRules : ITicTacToeRules
     {
         int ICountWin();
-
     }
+
+
     public class BOFField : ITicTacToeField
     {
         int[,] _Field = new int[3, 3] { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
@@ -57,13 +58,15 @@ namespace OOPGames.Classes.Gruppe_D
             set { _P2++; }
         }
     }
+
+
     public class BestOfFiveRulesD : ITicTacToeBOFRules
     {
 
-        BOFField _KrassesFeld = new BOFField();
+        BOFField _KrassesFeldBOF = new BOFField();
         public string Name { get { return "DieKrassenRegelnBOF"; } }
 
-        public IGameField CurrentField { get { return _KrassesFeld; } }
+        public IGameField CurrentField { get { return _KrassesFeldBOF; } }
 
         public bool MovesPossible
         {
@@ -73,7 +76,7 @@ namespace OOPGames.Classes.Gruppe_D
                 {
                     for (int j = 0; j < 3; j++)
                     {
-                        if (_KrassesFeld[i, j] == 0)
+                        if (_KrassesFeldBOF[i, j] == 0)
                         {
                             return true;
                         }
@@ -87,29 +90,27 @@ namespace OOPGames.Classes.Gruppe_D
         }
 
 
-
-
-        public ITicTacToeField TicTacToeField { get { return _KrassesFeld; } }
+        public ITicTacToeField TicTacToeField { get { return _KrassesFeldBOF; } }
 
         public int CheckIfPLayerWon()
         {
             for (int i = 0; i < 3; i++)
             {
-                if (_KrassesFeld[i, 0] == 1 && _KrassesFeld[i, 1] == 1 && _KrassesFeld[i, 2] == 1)
+                if (_KrassesFeldBOF[i, 0] == 1 && _KrassesFeldBOF[i, 1] == 1 && _KrassesFeldBOF[i, 2] == 1)
                 {
 
                     return 1;
                 }
-                if (_KrassesFeld[0, i] == 1 && _KrassesFeld[1, i] == 1 && _KrassesFeld[2, i] == 1) { return 1; }
-                if (_KrassesFeld[i, 0] == 2 && _KrassesFeld[i, 1] == 2 && _KrassesFeld[i, 2] == 2) { return 2; }
-                if (_KrassesFeld[0, i] == 2 && _KrassesFeld[1, i] == 2 && _KrassesFeld[2, i] == 2) { return 2; }
+                if (_KrassesFeldBOF[0, i] == 1 && _KrassesFeldBOF[1, i] == 1 && _KrassesFeldBOF[2, i] == 1) { return 1; }
+                if (_KrassesFeldBOF[i, 0] == 2 && _KrassesFeldBOF[i, 1] == 2 && _KrassesFeldBOF[i, 2] == 2) { return 2; }
+                if (_KrassesFeldBOF[0, i] == 2 && _KrassesFeldBOF[1, i] == 2 && _KrassesFeldBOF[2, i] == 2) { return 2; }
 
 
             }
-            if (_KrassesFeld[0, 0] == 1 && _KrassesFeld[1, 1] == 1 && _KrassesFeld[2, 2] == 1) { return 1; }
-            if (_KrassesFeld[2, 0] == 1 && _KrassesFeld[1, 1] == 1 && _KrassesFeld[0, 2] == 1) { return 1; }
-            if (_KrassesFeld[0, 0] == 2 && _KrassesFeld[1, 1] == 2 && _KrassesFeld[2, 2] == 2) { return 2; }
-            if (_KrassesFeld[2, 0] == 2 && _KrassesFeld[1, 1] == 2 && _KrassesFeld[0, 2] == 2) { return 2; }
+            if (_KrassesFeldBOF[0, 0] == 1 && _KrassesFeldBOF[1, 1] == 1 && _KrassesFeldBOF[2, 2] == 1) { return 1; }
+            if (_KrassesFeldBOF[2, 0] == 1 && _KrassesFeldBOF[1, 1] == 1 && _KrassesFeldBOF[0, 2] == 1) { return 1; }
+            if (_KrassesFeldBOF[0, 0] == 2 && _KrassesFeldBOF[1, 1] == 2 && _KrassesFeldBOF[2, 2] == 2) { return 2; }
+            if (_KrassesFeldBOF[2, 0] == 2 && _KrassesFeldBOF[1, 1] == 2 && _KrassesFeldBOF[0, 2] == 2) { return 2; }
             return -1;
         }
 
@@ -119,7 +120,7 @@ namespace OOPGames.Classes.Gruppe_D
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    _KrassesFeld[i, j] = 0;
+                    _KrassesFeldBOF[i, j] = 0;
                 }
             }
         }
@@ -137,7 +138,7 @@ namespace OOPGames.Classes.Gruppe_D
         {
             if (move.Row >= 0 && move.Row < 3 && move.Column >= 0 && move.Column < 3)
             {
-                _KrassesFeld[move.Row, move.Column] = move.PlayerNumber;
+                _KrassesFeldBOF[move.Row, move.Column] = move.PlayerNumber;
             }
         }
 

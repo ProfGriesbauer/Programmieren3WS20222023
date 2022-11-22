d5f67225ed31c20fdb702e85980631a53f87ee0c
Test <test@test.de>
2022-11-22 08:04:31 +0100

    Computer Player hinzugefügt

--- OOPGames/OOPGames/MainWindow.xaml.cs
+++ OOPGames/OOPGames/MainWindow.xaml.cs
@@ -68,6 +68,8 @@ namespace OOPGames
             //Players
             OOPGamesManager.Singleton.RegisterPlayer(new TicTacToeHumanPlayer());
             OOPGamesManager.Singleton.RegisterPlayer(new E_TicTacToeHumanPlayer());
+            OOPGamesManager.Singleton.RegisterPlayer(new E_TicTacToeComputerPlayer_easy());
+            OOPGamesManager.Singleton.RegisterPlayer(new E_TicTacToeComputerPlayer_middle());
             OOPGamesManager.Singleton.RegisterPlayer(new TicTacToeComputerPlayer());
             OOPGamesManager.Singleton.RegisterPlayer(new TicTacToeComputerPlayerD());
             OOPGamesManager.Singleton.RegisterPlayer(new GJ_TicTacToeHumanPlayer());

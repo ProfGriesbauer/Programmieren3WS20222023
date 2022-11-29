df6fed014419601892948c9e3247f16acd0736b4
Knoll <janis.knoll@stud.hs-kempten.de>
2022-11-29 09:37:51 +0100

    Interfaces Gruppe F added

---
+++ OOPGames/OOPGames/Classes/Gruppe_F/InterfacesGruppeF.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OOPGames
+{
+    public interface ITimeStamp
+    {
+        DateTime? LastUpdated { get; set; }
+    }
+
+    public interface IFieldSum
+    {
+        int? checkFieldSum { get;  }
+    }
+    public interface IFTicTacToeField : ITicTacToeField
+    {
+        int CurrentWinner { get;  set; }
+    }
+}

f575fcf287431dbc1785b2a4cc6200f8972b6774
Michael Scarvaglieri <michael.scarvaglieri@ke-tec.com>
2022-12-04 21:05:21 +0100

    Group K: Fixed InformationWindow for non blocking usage

--- OOPGames/OOPGames/Classes/Gruppe K/Form/K_InformationWindow.xaml.cs
+++ OOPGames/OOPGames/Classes/Gruppe K/Form/K_InformationWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -27,13 +28,28 @@ namespace OOPGames.Classes.Gruppe_K.Form
 
         public static void createInformationPopUp(String title, String text, bool blocksContext)
         {
+            if (blocksContext)
+            {
+
+               internCreateInformationPopUp(title, text);
+                
+            } else
+            {
+                Thread informationPopUp = new Thread(() => internCreateInformationPopUp(title, text));
+                informationPopUp.SetApartmentState(ApartmentState.STA);
+                informationPopUp.Start();
+   
+            }
+        }
+      
+        private static void internCreateInformationPopUp(String title, String text)
+        {
             K_InformationWindow window = new K_InformationWindow();
             window.Title = title;
             window.label.Content = text;
-            bool result = (bool)window.ShowDialog();
-            while (blocksContext&&result) ;
+            bool result= (bool)window.ShowDialog();
+            while (result) ;
         }
-      
 
     }
 }

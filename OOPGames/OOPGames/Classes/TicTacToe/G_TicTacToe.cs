dfa89e926f1f9e1acebcd93cc1984d6c770d17f5
Kkneiphs <kilian.kneip@stud.hs-kempten.de>
2022-12-05 21:33:48 +0100

    2. Versuch

--- OOPGames/OOPGames/Classes/TicTacToe/G_TicTacToe.cs
+++ OOPGames/OOPGames/Classes/TicTacToe/G_TicTacToe.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Text;
+using System.Text; 
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Forms;

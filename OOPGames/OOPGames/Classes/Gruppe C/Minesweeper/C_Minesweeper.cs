cb2a63b2d0486d53fa9851bc71b64c84e5f7175e
MERTL00 <martin.reschauer@stud.hs-kempten.de>
2022-12-05 15:21:22 +0100

    zugrefügt

--- OOPGames/OOPGames/Classes/Gruppe C/Minesweeper/C_Minesweeper.cs
+++ OOPGames/OOPGames/Classes/Gruppe C/Minesweeper/C_Minesweeper.cs
@@ -6,6 +6,44 @@ using System.Threading.Tasks;
 
 namespace OOPGames.Classes.Gruppe_C.Minesweeper
 {
+    public class C_MinesweeperField : C_IMinesweeperField
+    {
+        Segment[,] _Field = new Segment[16, 16] { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } };
+        Segment _PointsPlayer1;
+        Segment _PointsPlayer2;
+
+        public int this[Segment r, Segment c]
+        {
+            get
+            {
+                if (r >= 0 && r < 16 && c >= 0 && c < 16)
+                {
+                    return _Field[r, c];
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            set
+            {
+                if (r >= 0 && r < 16 && c >= 0 && c < 16)
+                {
+                    _Field[r, c] = value;
+                }
+            }
+        }
+
+        public int PointsPlayer1 { get { return _PointsPlayer1; } set { _PointsPlayer1 = value; } }
+        public int PointsPlayer2 { get { return _PointsPlayer2; } set { _PointsPlayer2 = value; } }
+
+        public bool CanBePaintedBy(IPaintGame painter)
+        {
+            return painter is C_IPaintTicTacToe;
+
+        }
+    }
+
     public class Segment
     {
         int Mine = 0;
@@ -18,16 +56,16 @@ namespace OOPGames.Classes.Gruppe_C.Minesweeper
                 return Mine;
             }
             else { return 0; }
-           
+
         }
         public int CountMines(int x, int y)
         {
             int count = 0;
-            for (int r=0; r < 3; r++)
+            for (int r = 0; r < 3; r++)
             {
-                for (int c=0; c<3;c++)
+                for (int c = 0; c < 3; c++)
                 {
-                    if (currentField[x-1+r,y-1+c].Segment.CheckMine() == 1)
+                    if (currentField[x - 1 + r, y - 1 + c].Segment.CheckMine() == 1)
                     {
                         count++;
                     }
@@ -36,5 +74,5 @@ namespace OOPGames.Classes.Gruppe_C.Minesweeper
             return count;
         }
     }
-    
+
 }

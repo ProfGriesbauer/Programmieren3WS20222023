7293046b1c9a67203ce921f8448bb782e6b83d9a
Test <test@test.de>
2022-12-05 17:07:48 +0100

    Computer Player eingefügt

---
+++ OOPGames/OOPGames/Interfaces/Gruppe E/I_E_4Gewinnt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace OOPGames.Interfaces.Gruppe_E
+{
+    public interface I_E_PaintVierGewinnt : IPaintGame
+    {
+        void PaintVierGewinntField(Canvas canvas, I_E_VierGewinntField currentField);
+    }
+    public interface I_E_VierGewinntField : IGameField
+    {
+        int this[int r, int c] { get; set; }
+
+    }
+
+    public interface I_E_VierGewinntRules : IGameRules
+    {
+        I_E_VierGewinntField VierGewinntField { get; }
+        void DoVierGewinntMove(I_E_VierGewinntMove move);
+    }
+    
+    
+    public interface I_E_VierGewinntMove : IRowMove, IColumnMove
+    {
+
+    }
+}

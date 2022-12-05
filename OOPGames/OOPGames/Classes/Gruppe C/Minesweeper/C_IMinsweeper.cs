6ca7c46bea881c12fadac6a1db36d4dae86bcf43
Criinix <102599308+Criinix@users.noreply.github.com>
2022-12-05 15:28:39 +0100

    Beginn interfaces Minesweeper und neue IClickselection für Erkennung ob LeftMouse oder RightMouse

--- OOPGames/OOPGames/Classes/Gruppe C/Minesweeper/C_IMinsweeper.cs
+++ OOPGames/OOPGames/Classes/Gruppe C/Minesweeper/C_IMinsweeper.cs
@@ -7,8 +7,42 @@ using System.Windows.Controls;
 
 namespace OOPGames.Classes.Gruppe_C.Minesweeper
 {
-    public interface C_IPaintMinesweeper:IPaintGame
+    public interface C_IPaintMinesweeper : IPaintGame2
     {
         void PaintMinesweeperField(Canvas canvas, C_IMinesweeperField currentField);
+        void TickPaintGameField(Canvas canvas, C_IMinesweeperField currentField);
+    }
+
+    public interface C_IMinesweeperField : IGameField
+    {
+        Segment this[Segment r,Segment c] { get; set; }
+    }
+    public interface C_IMinesweeperRules  : IGameRules
+    {
+        C_IMinesweeperField MinesweeperField { get; }
+
+        void DoMinesweeperMove(C_IMinesweeperMove move);
+    }
+    public interface C_IMinesweeperMove: IRowMove, IColumnMove
+    {
+
+    }
+    public interface C_IHumanMinesweeperPlayer : IHumanGamePlayer 
+    {
+        C_IMinesweeperField GetMove(IMoveSelection selection, C_IMinesweeperField field);
+    }
+    public interface C_IClickselection2 : IClickSelection
+    {
+        bool RightClick { get; set; }
+        bool LeftClick { get; set; }
+    }
+    public class C_ClickSelection : ClickSelection, C_IClickselection2
+    {
+        public C_ClickSelection (int clickX, int clickY, bool butt) : base(clickX, clickY)
+        {
+
+        }
+        public bool RightClick { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool LeftClick { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
 }
